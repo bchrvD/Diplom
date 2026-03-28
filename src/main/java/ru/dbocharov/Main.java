0c778b9cdf0c9ab346bbf75dbc4132a60a2bd5d8
dbocharov <dmitriyBocharov4@yandex.ru>
2026-03-28 18:25:30 +0300

    предпоследний коммит перед рисованием gui

--- src/main/java/ru/dbocharov/Main.java
+++ src/main/java/ru/dbocharov/Main.java
@@ -21,13 +21,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Map<Double, String> valueInPoint = new HashMap<>(); //значения f в TOKAK x
+        Map<Double, String> valueInPoint = new HashMap<>();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите количество точек разбиения: ");
         int countPoint = scanner.nextInt();
-        int n = countPoint + 1; // количество интервалов
+        int n = countPoint + 1;
         System.out.println("Введите функцию от x и t (например: sin(x)*t + x^2):");
         scanner.nextLine();
         String inputFunctionXT = scanner.nextLine();
@@ -36,91 +36,63 @@ public class Main {
             inputFunctionXT = "2*"+Math.PI+"*sin(2*"+Math.PI+"*x)*(2*"+ Math.PI+"*cos(2*"+Math.PI+"*t)-sin(2*"+Math.PI+"*t))";
         }
 
-        for (int i = 0; i <= countPoint + 1; i++) {
-            valueInPoint.put((double) i / (double) (countPoint + 1),
-                    inputFunctionXT.replaceAll("t",
-                            String.valueOf((double) i / (double)
-                                    (countPoint + 1))));
+        for (int i = 0; i <= n; i++) {
+            valueInPoint.put((double) i / n,
+                    inputFunctionXT.replaceAll("(?<![a-zA-Z])t(?![a-zA-Z])", String.valueOf((double) i / n)));
         }
+
         System.out.println("Введите количество базисных функций omega: ");
-        int m = scanner.nextInt(); //количество омег
+        int m = scanner.nextInt();
         List<String> omegas = new ArrayList<>();
-        for (int i = 1; i<=m;i++){
+        for (int i = 1; i <= m; i++) {
             omegas.add(omegaGenerator(i));
         }
-        String omega1 = "x*(x-1)";
-        String omega2 = "x^2*(x-1)";
 
-        double tau = 1d / (countPoint + 1);
-
-        double[][] matrix = new double[(n+1)*m][m*(n+1)];
-
+        double tau = 1d / n;
+        double[][] matrix = new double[(n + 1) * m][m * (n + 1)];
         UnivariateIntegrator integrator = new SimpsonIntegrator();
-        //Заполннение матрицы
+
+        // Заполнение базовых блоков матрицы
         for (int i = 0; i < m; i++) {
             final int powI = i + 1;
             for (int j = 0; j < m; j++) {
                 final int powJ = j + 1;
-                UnivariateFunction productFunc = x -> {
-                    double omegaI = Math.pow(x, powI) * (x - 1);
-                    double omegaJ = Math.pow(x, powJ) * (x - 1);
-                    return omegaI * omegaJ;
-                };
-                UnivariateFunction derivativeFunc = x -> {
-                    double dOmegaI = (powI + 1) * Math.pow(x, powI) - powI * Math.pow(x, powI - 1);
-                    double dOmegaJ = (powJ + 1) * Math.pow(x, powJ) - powJ * Math.pow(x, powJ - 1);
-                    return dOmegaI * dOmegaJ;
-                };
-                double integral1 = integrator.integrate(1000, productFunc, 0, 1);
-                double integral2 = integrator.integrate(1000, derivativeFunc, 0, 1);
-                matrix[i][j] = -1.0 / tau * integral1 + 0.5 * integral2;
+                UnivariateFunction productFunc = x -> (Math.pow(x, powI) * (x - 1)) * (Math.pow(x, powJ) * (x - 1));
+                UnivariateFunction derivativeFunc = x -> ((powI + 1) * Math.pow(x, powI) - powI * Math.pow(x, powI - 1)) *
+                        ((powJ + 1) * Math.pow(x, powJ) - powJ * Math.pow(x, powJ - 1));
+
+                double integral1 = integrator.integrate(1000000, productFunc, 0, 1);
+                double integral2 = integrator.integrate(1000000, derivativeFunc, 0, 1);
+
+                matrix[i][j] = -1.0 / tau * integral1 + 0.5 * integral2; // Блок A
+                matrix[i][j + m] = 1.0 / tau * integral1 + 0.5 * integral2; // Блок B
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            final int powI = i + 1;
-            for (int j = m; j < 2 * m; j++) {
-                final int powJ = j + 1 - m;
-                UnivariateFunction productFunc = x -> {
-                    double omegaI = Math.pow(x, powI) * (x - 1);
-                    double omegaJ = Math.pow(x, powJ) * (x - 1);
-                    return omegaI * omegaJ;
-                };
-                UnivariateFunction derivativeFunc = x -> {
-                    double dOmegaI = (powI + 1) * Math.pow(x, powI) - powI * Math.pow(x, powI - 1);
-                    double dOmegaJ = (powJ + 1) * Math.pow(x, powJ) - powJ * Math.pow(x, powJ - 1);
-                    return dOmegaI * dOmegaJ;
-                };
-                double integral1 = integrator.integrate(1000, productFunc, 0, 1);
-                double integral2 = integrator.integrate(1000, derivativeFunc, 0, 1);
-                matrix[i][j] = 1.0 / tau * integral1 + 0.5 * integral2;
-            }
-        }
-        // Переносим элементы на нужные места (создаем лесенку)
+        // Перенос элементов ("лесенка")
         for (int k = 1; k < n; k++) {
             int shift = k * m;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < m; j++) {
-                    // Копируем блок A
                     matrix[shift + i][shift + j] = matrix[i][j];
-                    // Копируем блок B
                     if (shift + m + j < matrix[0].length) {
                         matrix[shift + i][shift + m + j] = matrix[i][j + m];
                     }
                 }
             }
         }
-        // Заполнение периодического условия
+
+        // Периодическое условие
         for (int i = 0; i < m; i++) {
             int row = n * m + i;
             matrix[row][i] = 1.0;
             matrix[row][n * m + i] = -1.0;
         }
 
-        System.out.println("МАТРИЦА:");
+        // Выволд матрицы
+        System.out.println("Матрица:");
         int rows = matrix.length;
         int cols = matrix[0].length;
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.printf("%10.4f ", matrix[i][j]);
@@ -128,11 +100,10 @@ public class Main {
             System.out.println();
         }
 
-
         // Заполнение правого вектора
         double[] rightVector = new double[(n + 1) * m];
-        double a = 0d; // Нижний предел интегрирования
-        double b = 1d; // Верхний предел интегрирования
+        double a = 0d;
+        double b = 1d;
         for (int k = 1; k <= n; k++) {
             double t_k = (double) k / n;
             double t_k_minus_1 = (double) (k - 1) / n;
@@ -142,42 +113,40 @@ public class Main {
             for (int i = 0; i < m; i++) {
                 String currentOmega = omegas.get(i);
                 Expression expression = new ExpressionBuilder("(" + f_averaged_str + ")*(" + currentOmega + ")")
-                        .variable("x")
-                        .build();
+                        .variable("x").build();
                 UnivariateFunction function = x -> {
                     expression.setVariable("x", x);
                     return expression.evaluate();
                 };
-                double integral = integrator.integrate(1000, function, a, b);
-                rightVector[(k - 1) * m + i] = integral;
+                rightVector[(k - 1) * m + i] = integrator.integrate(1000000, function, a, b);
             }
         }
-        System.out.println("ПРАВЫЙ ВЕКТОР: ");
+
+        // Вывод вектора
+        System.out.println("Правый вектор: ");
         System.out.println(Arrays.toString(rightVector));
 
-        // Создаем объекты матрицы и вектора
         RealMatrix coefficients = MatrixUtils.createRealMatrix(matrix);
         RealVector constants = MatrixUtils.createRealVector(rightVector);
-
-        //создаем решатель и решаем слу
         DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
         RealVector solution = solver.solve(constants);
 
-        Map<Double, String> solutionOfTask = new HashMap<>(); //Значения U(h k)
-        int j = 0;
-        for (int i = 0; i <= countPoint + 1; i++) {
-            solutionOfTask.put((double) i / (double) (countPoint + 1),
-                    "(" + solution.getEntry(j++) + ")*(" + omega1 + ")+(" +
-                            solution.getEntry(j++) + ")*(" + omega2 + ")");
+        // Сборка решения
+        Map<Double, String> solutionOfTask = new HashMap<>();
+        for (int i = 0; i <= n; i++) {
+            String expr = "";
+            for (int k = 0; k < m; k++) {
+                if (k > 0) expr += "+";
+                expr += "(" + solution.getEntry(i * m + k) + ")*(" + omegas.get(k) + ")";
+            }
+            solutionOfTask.put((double) i / n, expr);
         }
 
-        Map<Double, String> currentMap = new HashMap<>(); //Точное решение
-        String currentFunctionXT = "sin(2*"+Math.PI+"*x)*cos(2*"+Math.PI+"*t)";
-        for (int i = 0; i <= countPoint + 1; i++) {
-            currentMap.put((double) i / (double) (countPoint + 1),
-                    currentFunctionXT.replaceAll("t",
-                            String.valueOf((double) i / (double)
-                                    (countPoint + 1))));
+        Map<Double, String> currentMap = new HashMap<>();
+        String currentFunctionXT = "sin(2*"+Math.PI+"*x)*cos(2*"+Math.PI+"*t)"; //точное решение для test - кейса
+        for (int i = 0; i <= n; i++) {
+            currentMap.put((double) i / n,
+                    currentFunctionXT.replaceAll("(?<![a-zA-Z])t(?![a-zA-Z])", String.valueOf((double) i / n)));
         }
 
         plotExactAndApproxSolutions(solutionOfTask, currentMap);
@@ -185,7 +154,10 @@ public class Main {
 
     public static void plotExactAndApproxSolutions(Map<Double, String> approxMap, Map<Double, String> exactMap) {
         double globalMaxDiff = 0;
-        for (Double t : approxMap.keySet()) {
+        List<Double> sortedKeys = new ArrayList<>(approxMap.keySet());
+        Collections.sort(sortedKeys);
+
+        for (Double t : sortedKeys) {
             String approxExprStr = approxMap.get(t);
             String exactExprStr = exactMap.get(t);
 
@@ -198,15 +170,8 @@ public class Main {
             chart.getStyler().setLegendVisible(true);
             chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
 
-            Expression approxExpr;
-            Expression exactExpr;
-            try {
-                approxExpr = new ExpressionBuilder(approxExprStr).variable("x").build();
-                exactExpr = new ExpressionBuilder(exactExprStr).variable("x").build();
-            } catch (Exception e) {
-                System.out.println("Ошибка при разборе выражения при t = " + t);
-                continue;
-            }
+            Expression approxExpr = new ExpressionBuilder(approxExprStr).variable("x").build();
+            Expression exactExpr = new ExpressionBuilder(exactExprStr).variable("x").build();
 
             int points = 100;
             double[] xData = new double[points];
@@ -217,16 +182,11 @@ public class Main {
             for (int i = 0; i < points; i++) {
                 double x = i / (double) (points - 1);
                 xData[i] = x;
-                approxExpr.setVariable("x", x);
-                exactExpr.setVariable("x", x);
-                double approxVal = approxExpr.evaluate();
-                double exactVal = exactExpr.evaluate();
+                double approxVal = approxExpr.setVariable("x", x).evaluate();
+                double exactVal = exactExpr.setVariable("x", x).evaluate();
                 yApprox[i] = approxVal;
                 yExact[i] = exactVal;
-                double diff = Math.abs(approxVal - exactVal);
-                if (diff > maxDiff) {
-                    maxDiff = diff;
-                }
+                maxDiff = Math.max(maxDiff, Math.abs(approxVal - exactVal));
             }
             globalMaxDiff = Math.max(globalMaxDiff, maxDiff);
 
